ed32fd390b0a0898e4a2c1dec682881659734f51
Krunalwani <krunalrwani2884@gmail.com>
2026-02-10 20:39:47 +0530

    Structure

---
+++ Team3-project-backend/src/test/java/com/team3/capstone/backend/Team3ProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team3.capstone.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team3ProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
