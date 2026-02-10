66332356031f8ec36dcdbc014bf223ef2c72deee
krunal wani <krunalrwani2884@gmail.com>
2026-02-10 22:54:14 +0530

    Structure

---
+++ Team3-project-backend/src/main/java/com/team3/capstone/backend/Team3ProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.team3.capstone.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team3ProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team3ProjectBackendApplication.class, args);
+	}
+
+}
---
+++ Team3-project-backend/src/main/java/com/team3/capstone/backend/entity/User.java
@@ -0,0 +1,5 @@
+package com.team3.capstone.backend.entity;
+
+public class User {
+
+}
